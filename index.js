5cb3536b6fa569c6cab5b88de0fbaf6e9238973d
rizwan-rana <rizwan@ranagroup.com.au>
2025-05-24 06:10:22 +1000

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import ChatDashboard from './ChatDashboard';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import ChatDashboard from "./App";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<ChatDashboard />);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <ChatDashboard />
+  </React.StrictMode>
+);
